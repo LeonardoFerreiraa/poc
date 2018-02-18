0b3a7802c2a9a5eeb00b2e19b3e7d7cdaf861418
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-02-18 12:57:06 -0300

    poc oauth basic

---
+++ poc-oauth2/basic/src/main/java/br/com/leonardoferreira/pocoauth2/Greet.java
@@ -0,0 +1,17 @@
+package br.com.leonardoferreira.pocoauth2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by lferreira on 2/17/18
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greet {
+
+    private String name;
+
+}
---
+++ poc-oauth2/basic/src/test/java/br/com/leonardoferreira/pocoauth2/TestOauth2.java
@@ -0,0 +1,37 @@
+package br.com.leonardoferreira.pocoauth2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by lferreira on 2/17/18
+ */
+@RunWith(SpringRunner.class)
+public class TestOauth2 {
+    @Test
+    public void test() {
+        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+        resource.setUsername("guest");
+        resource.setPassword("guest123");
+        resource.setAccessTokenUri("http://localhost:8080/oauth/token");
+        resource.setClientId("trustedclient");
+        resource.setClientSecret("trustedclient123");
+        resource.setGrantType("password");
+        resource.setScope(Collections.singletonList("openid"));
+
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+        Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+        Assertions.assertThat(greet.getName())
+                .isEqualTo("Hello World!");
+
+    }
+}
