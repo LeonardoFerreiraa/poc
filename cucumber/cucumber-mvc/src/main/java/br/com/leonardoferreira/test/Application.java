17bb1f76abccbb3ce4c2d03186746d475fbb3d8d
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-02-18 21:56:06 -0300

    crud e auth

--- cucumber/cucumber-mvc/src/main/java/br/com/leonardoferreira/test/Application.java
+++ cucumber/cucumber-mvc/src/main/java/br/com/leonardoferreira/test/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,6 +23,7 @@ public class Application {
 	}
 
 	@Bean
+    @Profile("!test")
 	public CommandLineRunner createUser(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             String username = "teste@email.com";
