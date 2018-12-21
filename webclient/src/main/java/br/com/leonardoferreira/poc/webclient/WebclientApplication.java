db6491172f92bf39134aa4fc3dcf003dd8c6b2ef
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-12-20 22:26:38 -0200

    webclient poc

---
+++ webclient/src/main/java/br/com/leonardoferreira/poc/webclient/WebclientApplication.java
@@ -0,0 +1,14 @@
+package br.com.leonardoferreira.poc.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientApplication.class, args);
+	}
+
+}
+
